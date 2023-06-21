452946eba23adcf0a1dcffa5d5c02e693e358fc3
Kristen Swaim <106117219+kdswaim@users.noreply.github.com>
2023-06-21 19:14:59 -0400

    fixed GenreListItem

--- GameFinder.Models/Genres/GenreListItem.cs
+++ GameFinder.Models/Genres/GenreListItem.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,8 @@ namespace GameFinder.Models.Genres
 {
     public class GenreListItem
     {
-        
+        public int Id {get; set;}
+        public string Name {get; set;}
+        public string Description {get; set;}
     }
 }
