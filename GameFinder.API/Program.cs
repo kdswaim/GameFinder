62cb7c187d95655a070b4c1a107484f4792c5678
KTerew <Terewkody@gmail.com>
2023-06-22 19:49:25 -0400

    small changes

--- GameFinder.API/Program.cs
+++ GameFinder.API/Program.cs
@@ -9,9 +9,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<ApplicationDbContext>(options => {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
