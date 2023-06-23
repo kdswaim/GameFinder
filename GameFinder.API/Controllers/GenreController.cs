c79a42776f67b1c3f4a10ed9eebc8267eab4e8ad
Kristen Swaim <106117219+kdswaim@users.noreply.github.com>
2023-06-22 20:37:22 -0400

    built genrecontroller

---
+++ GameFinder.API/Controllers/GenreController.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GameFinder.Services.Genres;
+using Microsoft.AspNetCore.Mvc;
+using GameFinder.Models.Genres;
+
+namespace GameFinder.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GenreController : ControllerBase
+    {
+        private readonly IGenreServices _gService;
+        public GenreController(IGenreServices gService)
+        {
+            _gService = gService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _gService.GetGenres());
+        }
+
+        [HttpGet("(id:int)")]
+        public async Task<IActionResult> Get(int id)
+        {
+            return Ok(await _gService.GetGenreById(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(GenreCreate model)
+        {
+            if(ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if(await _gService.CreateGenre(model))
+            {
+                return Ok("Success!");
+            }
+            else
+            return StatusCode(500, "Internal Server Error.");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put(GenreEdit model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (await _gService.UpdateGenre(model))
+            {
+                return Ok("Success!");
+            }
+            else {
+                return StatusCode(500, "Internal Server Error.");
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Delete( int id)
+        {
+            if (id<=0)
+            {
+                return BadRequest("Invalid Id.");
+            }
+            if (await _gService.DeleteGenre(id))
+            {
+                return Ok("Success!");
+            }
+            else {
+                return StatusCode(500, "Internal Server Error.");
+            }
+        }
+
+    }
+}
